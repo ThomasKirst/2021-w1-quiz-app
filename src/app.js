bc13314c2ea1396cc247295e095e6974b3c4f1fc
Thomas Kirst <thomas.kirst@neuefische.de>
2021-02-03 09:49:49 +0100

    Added toggle answer functionality

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-console.log('Hello!');
+// document
+//   .querySelector('#button1')
+//   .addEventListener('click', () =>
+//     document.querySelector('#answer1').classList.toggle('hidden')
+//   );
 
 document
-  .getElementById('cool-button')
-  .addEventListener('click', () => console.log('clicked'));
+  .querySelectorAll('button')
+  .forEach((button) => button.addEventListener('click', toggleAnswer));
+
+function toggleAnswer(button) {
+  button.parentNode.querySelector('p').classList.toggle('hidden');
+}
